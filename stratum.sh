3104fc496acfc9a7ab0dcb58b645077055bc705e
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2020-11-08 04:54:43 +0100

    Add second stratum

--- stratum.sh
+++ stratum.sh
@@ -22,12 +22,22 @@ if [[ ("$AutoExchange" == "yes") ]]; then
 sudo sed -i 's/CFLAGS += -DNO_EXCHANGE/#CFLAGS += -DNO_EXCHANGE/' $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum/Makefile
 fi
 hide_output sudo make
+cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum-yescrypt/iniparser
+hide_output sudo make
+cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum-yescrypt
+if [[ ("$AutoExchange" == "yes") ]]; then
+sudo sed -i 's/CFLAGS += -DNO_EXCHANGE/#CFLAGS += -DNO_EXCHANGE/' $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum_yescrypt/Makefile
+fi
+hide_output sudo make
 
 echo -e " Building stratum folder structure and copying files...$COL_RESET"
 cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum
 sudo cp -a config.sample/. $STORAGE_ROOT/yiimp/site/stratum/config
 sudo cp -r stratum $STORAGE_ROOT/yiimp/site/stratum
 sudo cp -r run.sh $STORAGE_ROOT/yiimp/site/stratum
+cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum-yescrypt
+sudo mv stratum $STORAGE_ROOT/yiimp/site/stratum/stratum_yescrypt
+sudo mv run.sh $STORAGE_ROOT/yiimp/site/stratum/run_yescrypt.sh
 cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
 sudo cp -r $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/blocknotify/blocknotify $STORAGE_ROOT/yiimp/site/stratum
 sudo cp -r $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/blocknotify/blocknotify /usr/bin
@@ -46,6 +56,19 @@ done
 exec bash' | sudo -E tee $STORAGE_ROOT/yiimp/site/stratum/config/run.sh >/dev/null 2>&1
 sudo chmod +x $STORAGE_ROOT/yiimp/site/stratum/config/run.sh
 
+echo '#!/usr/bin/env bash
+source /etc/yiimpserver.conf
+source $STORAGE_ROOT/yiimp/.yiimp.conf
+ulimit -n 10240
+ulimit -u 10240
+cd '""''"${STORAGE_ROOT}"''""'/yiimp/site/stratum
+while true; do
+./stratum_yescrypt config/$1
+sleep 2
+done
+exec bash' | sudo -E tee $STORAGE_ROOT/yiimp/site/stratum/config/run_yescrypt.sh >/dev/null 2>&1
+sudo chmod +x $STORAGE_ROOT/yiimp/site/stratum/config/run_yescrypt.sh
+
 sudo rm -r $STORAGE_ROOT/yiimp/site/stratum/run.sh
 echo '#!/usr/bin/env bash
 source /etc/yiimpserver.conf
@@ -53,6 +76,14 @@ source $STORAGE_ROOT/yiimp/.yiimp.conf
 cd '""''"${STORAGE_ROOT}"''""'/yiimp/site/stratum/config/ && ./run.sh $*
 ' | sudo -E tee $STORAGE_ROOT/yiimp/site/stratum/run.sh >/dev/null 2>&1
 sudo chmod +x $STORAGE_ROOT/yiimp/site/stratum/run.sh
+
+sudo rm -r $STORAGE_ROOT/yiimp/site/stratum/run_yescrypt.sh
+echo '#!/usr/bin/env bash
+source /etc/yiimpserver.conf
+source $STORAGE_ROOT/yiimp/.yiimp.conf
+cd '""''"${STORAGE_ROOT}"''""'/yiimp/site/stratum/config/ && ./run_yescrypt.sh $*
+' | sudo -E tee $STORAGE_ROOT/yiimp/site/stratum/run_yescrypt.sh >/dev/null 2>&1
+sudo chmod +x $STORAGE_ROOT/yiimp/site/stratum/run_yescrypt.sh
 
 echo -e " Updating stratum config files with database connection info...$COL_RESET"
 cd $STORAGE_ROOT/yiimp/site/stratum/config
